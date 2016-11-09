e6700769356e43a92a35bf26620773f1bfeb5c1d
nwulkow <nwulkow@mi.fu-berlin.de>
2016-11-09 12:00:58 +0100

    doppelte geloescht

--- EMstep.m
+++
@@ -1,5 +0,0 @@
-function X = EMstep(X0, deltaT, b,sigma)
-
-    X = X0 + deltaT*b(X0) + sqrt(deltaT)*sigma(X0)*randn(1,1);
-    
-    end
--- mvepdf.m
+++
@@ -1,6 +0,0 @@
-function p = mvepdf(X,lambda)
-
-[n,m] = size(X);
-for i = 1:n
-    p(i) = prod(exppdf(X(i,:), lambda));
-end
